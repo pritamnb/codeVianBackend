274a8a05417c83b0bdab2ae636cc07a9fe2500f6
Pritam Bhalerao <pritam.bhalerao@codevian.com>
2020-07-21 08:13:13 +0530

    test cicd

--- models/register.js
+++ models/register.js
@@ -2,29 +2,30 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const Joi = require('joi');
 const config = require('config');
+// testcicd
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     minlength: 3,
-    maxlength: 50
+    maxlength: 50,
   },
   email: {
     type: String,
     unique: true,
     minlength: 5,
     maxlength: 50,
-    required: true
+    required: true,
   },
   password: {
     type: String,
     required: true,
     minlength: 8,
-    maxlength: 1024
+    maxlength: 1024,
   },
-  isAdmin: Boolean
+  isAdmin: Boolean,
 });
-userSchema.methods.generateAuthToken = function() {
+userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign(
     { _id: this._id, isAdmin: this.isAdmin },
     config.get('jwtPrivateKey')
@@ -35,18 +36,9 @@ const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
   const schema = {
-    name: Joi.string()
-      .min(3)
-      .max(50)
-      .required(),
-    email: Joi.string()
-      .min(5)
-      .max(50)
-      .required(),
-    password: Joi.string()
-      .min(8)
-      .max(255)
-      .required()
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(5).max(50).required(),
+    password: Joi.string().min(8).max(255).required(),
   };
 
   return Joi.validate(user, schema);
